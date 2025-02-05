d6fcb2b75f3ede7919f3b02b87ae80c1eb6eb7be
sheranii <194492330+sheranii@users.noreply.github.com>
2025-02-05 15:12:45 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
-#define  BITS sizeof( unsigned int)*16
 
 int main(){
-     unsigned int a;
-     unsigned int msb;
-    scanf("%u",&a);
-    msb=1<<(BITS-1);
-    if ((num & msb)==1){
-        printf("Set");
-    }
-    else{
-        printf("Not Set");
-    }
-
-
+    int x ; 
+    scanf("%d" , &x);
+    (x & (1<<32)) ? printf("Set") : printf("Not Set");
+    return 0;
 }
