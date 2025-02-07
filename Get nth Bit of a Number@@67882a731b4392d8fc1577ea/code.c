420e444478de5ea1773139fdca8635c38171599e
sheranii <194492330+sheranii@users.noreply.github.com>
2025-02-07 14:12:02 +0530

    Solution to Get nth Bit of a Number

--- Get nth Bit of a Number@@67882a731b4392d8fc1577ea/code.c
+++ Get nth Bit of a Number@@67882a731b4392d8fc1577ea/code.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
-int main(){
-    int num;
-    int n;
-    n=0 | n=1;
-    scanf("%d",&num);
-    scanf("%d",&n);
-    int bitstatus;
-    bitstatus=(num>>1)&1;
-    printf("%d",bitstatus);
+#include <string.h>
+int main() {
+    char binarySequence[100];  
+    int n;                  
+    // Input the binary sequence as a string
+    printf("Enter a binary sequence (only 0s and 1s): ");
+    scanf("%s", binarySequence);
+
+    // Input the position (1-based index)
+    printf("Enter the position (n): ");
+    scanf("%d", &n);
+
+    // Check if the position is valid
+    if (n > 0 && n <= strlen(binarySequence)) {
+        // Output the nth value (1-based indexing)
+        printf("The %dth value is: %c\n", n, binarySequence[n-1]);
+    } else {
+        printf("Invalid position\n");
+    }
+
+    return 0;
 }
