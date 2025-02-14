519405a62c101c84ab4aaa30480042efc6f9e804
sheranii <194492330+sheranii@users.noreply.github.com>
2025-02-14 12:35:28 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,30 +4,28 @@ int main() {
     double a;
     double b;
     char c;
-
     scanf("%lf", &a);  // Use %lf for double
     scanf("%lf", &b);  // Use %lf for double
     scanf(" %c", &c);  // Add a space before %c to handle new line characters from previous input
-
     switch(c) {
         case '+':
-            printf("%lf\n", a + b);  // Use %lf to print double
+            printf("%0.lf", a + b);  
             break;
         case '-':
-            printf("%lf\n", a - b);  // Use %lf to print double
+            printf("%0.lf", a - b);  // Use %lf to print double
             break;
         case '*':
-            printf("%lf\n", a * b);  // Use %lf to print double
+            printf("%0.lf", a * b);  // Use %lf to print double
             break;
         case '/':
             if(b == 0) {
-                printf("error\n");
+                printf("error");
             } else {
-                printf("%lf\n", a / b);  // Use %lf to print double
+                printf("%0.lf", a / b);  // Use %lf to print double
             }
             break;
         default:
-            printf("error\n");
+            printf("error");
     }
     
     return 0;
