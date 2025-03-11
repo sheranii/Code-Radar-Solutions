56f17ad10e6ff96eada328d8f90ecf7c87dd5e79
sheranii <194492330+sheranii@users.noreply.github.com>
2025-03-11 22:43:02 +0530

    Solution to Search Student by Roll Number

---
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    int rollno;
+    char name[50];
+    float marks;
+    for(int i=1; i<=N; i++){
+        scanf("%d",&rollno);
+        scanf("%s",name);
+        scanf("%f",&marks);
+    }
+    int searchedrollno;
+    scanf("%d",&searchedrollno);
+    int find=0;
+    for(int i=0; i<N; i++){
+        if(rollno==searchedrollno){
+            printf("Roll Number: %d,",rollno);
+            printf("Name: %s,",name);
+            printf("Marks:%.2f",marks);
+            find=1;
+            break;
+        }
+    }
+    if(!find){
+        printf("Student not found");
+    }
+}
