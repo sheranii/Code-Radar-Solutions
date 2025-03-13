7005f5379642dfab820dda585803d8a4012cf84c
sheranii <194492330+sheranii@users.noreply.github.com>
2025-03-13 14:28:48 +0530

    Solution to Movie Ticket Booking System

--- Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
+++ Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
@@ -3,10 +3,14 @@ int main(){
     int N;
     scanf("%d",&N);
     int std,pre,vip;
+    float tstd,tpre,tvip;
     for(int i=1; i<=N;i++){
     scanf("%d",&std);
     scanf("%d",&pre);
     scanf("%d",&vip);
+    tsd+=std;
+    tpre+=pre;
+    tvip+=vip;
     }
     printf("Standard: %.2f, ",std);
     printf("Premium: %.2f, ",pre);
